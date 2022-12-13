89f6c367597a346a21d3d767c8d25522d7cea237
Vishal Thukral <vishalt@verneek.com>
2022-12-12 21:27:07 -0500

    code cleanuo

--- index.js
+++ index.js
@@ -30,29 +30,29 @@ async function syncConfig(endpoint, username, password, config) {
     return response;
 }
 
-async function padWithFiles(config) {
-    let body = JSON.parse(config);
-    let newItems = [];
-    let response = { appId: body["appId"] };
-    for (index in body["items"]) {
-      let item = body["items"][index];
-      if ("path" in item) {
-        let path = item["path"];
-        try {
-          let configDir = "~/workspace";
-          const data = await fs.readFile(join(configDir, path), {encoding: "utf-8",});
-          newItems.push({
-            keySuffix: item["keySuffix"],
-            value: data,
-          });
-        } catch (error) {
-            core.setFailed(error.message);
+async function padWithFiles(configDir, config) {
+    const body = JSON.parse(config);
+    const newItems = [];
+    const response = { appId: body["appId"] };
+    for (const item of body["items"]) {
+        if ("path" in item) {
+            const path = item["path"];
+            try {
+                const data = await fs.readFile(join(configDir, path), { encoding: "utf-8", });
+                newItems.push({
+                    keySuffix: item["keySuffix"],
+                    value: data,
+                });
+            } catch (error) {
+                core.setFailed(error.message);
+            }
+        } else {
+            newItems.push(item);
         }
-      } else newItems.push(item);
     }
-    response["items"] = newItems;  
+    response["items"] = newItems;
     return response;
-  }
+}
 
 
 (async () => {
@@ -65,14 +65,13 @@ async function padWithFiles(config) {
         const files = await fs.readdir(configDir, {
             withFileTypes: true
         })
-
         // @TODO use parallelization here
         for (const file of files) {
             if (file.isFile && file.name.includes('.json')) {
-                const data = await fs.readFile(join(configDir, file.name), { encoding: 'utf-8' })
-                const afterCheckPath = await padWithFiles(data)
-                const afterCheckPathStr = JSON.stringify(afterCheckPath)
-                const response = await syncConfig(endpoint, username, password, afterCheckPathStr)
+                console.log(`Processing ${file.name}`)
+                let data = await fs.readFile(join(configDir, file.name), { encoding: 'utf-8' })
+                data = JSON.stringify(await padWithFiles(configDir, data))
+                const response = await syncConfig(endpoint, username, password, data)
                 if (response.status > 299) {
                     throw new Error(`Failed with http code ${result.statusCode}`)
                 }
